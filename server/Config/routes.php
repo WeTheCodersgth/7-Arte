65d0e5622d569da527e1d02fb604e56c364f0d0f
We, The Coders <wethecoders01@gmail.com>
2025-11-17 00:23:57 +0100

    Create routes.php

---
+++ server/Config/routes.php
@@ -0,0 +1,26 @@
+<?php
+/** @var \Bramus\Router\Router $router Instância do router vinda da classe Aplicacao */
+$router = $this->roteador->getInstancia();
+
+// Define a base path para a config
+$router->setBasePath('/config');
+
+// Rota de teste
+$router->get('/', function() {
+    \App\Core\Response::json(['mensagem' => 'Bem-vindo à API v1 da We, The Coders!']);
+});
+
+$router->get('index', function() {
+    \App\Core\Response::json(['mensagem' => 'Bem-vindo à API v1 da We, The Coders!']);
+});
+
+// Grupo de rotas de autenticação
+$router->mount('/autenticacao', function() use ($router) {
+   
+    $router->post('/cadastrar', 'AuthController@cadastrar');
+});
+
+// Handler para rotas não encontradas dentro da API
+$router->set404(function() {
+    \App\Core\Response::erro('Endpoint não encontrado.', 404);
+});
